5ba07d3c33d131efd3058ff4152a827431b3043b
Erdi Akyüz <erdi.akyuz@alten.nl>
2020-05-14 14:49:58 +0200

    Csharpis pass by value test

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,6 +6,23 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void CSharpIsPassbyValue()
+        {   
+
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1,"New Name");
+
+            Assert.Equal("Book 1",book1.Name);
+
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+            book=new Book(name);
+        }
+
+
+        [Fact]
         public void CanSetNameFromReference()
         {   
 
@@ -13,7 +30,6 @@ namespace GradeBook.Tests
             SetName(book1,"New Name");
 
             Assert.Equal("New Name",book1.Name);
-        
 
         }
 
